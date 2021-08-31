871445c011eedcad6268ef90a2ca76e783b10059
Jumanohe <52806001+Jumanohe@users.noreply.github.com>
2021-08-30 21:52:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	var principal= document.getElementById("principal").value;
+  if(principal<=0){
+  	alert("Please enter a positive number")
+    document.getElementById("principal").focus();
+    return
+  }
+  var rate= document.getElementById("rate").value;
+  var years=document.getElementById("years").value;
+  var year=new Date().getFullYear()+parseInt(years);
+  var interest=principal*rate*years/100;
+  document.getElementById("result").innerText="If you deposit "+principal+", at an interest rate of "+rate+"%. \n You will recieve an aumont of "+interest+ "\n in the year "+ year+".\n" ;
+  
+}
+
+function updateRate()
+{
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
